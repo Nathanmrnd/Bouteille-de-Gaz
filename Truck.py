21d18627c239fbc02dca5758187af89ad7a74c03
Alphonse Raynaud <alphonse.raynaud@etu.minesparis.psl.eu>
2025-01-31 11:52:16 +0100

    fonction load_at_plant

--- Truck.py
+++ Truck.py
@@ -10,18 +10,31 @@ class Truck:
 
 
     def load_at_plant(self):
-        # if self.destination.stock >= 100-self.bouteilles:
-        #     self.bouteilles = 100
-        #     self.destination.stock -= 100-self.bouteilles
+        if self.full_bottles + self.destination.full_bottles <= 80:
+            self.full_bottles += self.destination.full_bottles
+            self.destination.full_bottles = 0
+        else:
+            self.destination.full_bottles = self.destination.full_bottles + self.full_bottles - 80
+            self.full_bottles = 80
+
+        if self.empty_bottles + self.destination.empty_bottles <= self.destination.capacity - 1: # pour la bouteille en cours de remplissage
+            self.destination.empty_bottles += self.empty_bottles
+            self.empty_bottles = 0
+        else:
+            self.empty_bottles = self.destination.empty_bottles + self.empty_bottles - (self.capacity - 1)
+            self.destination.empty_bottles = self.destination.capacity - 1
+
+        # Rq : les deux if ne peuvent pas être faux car sinon le camion ou l'usine déborderait déjà
+        
 
 
     def unload_at_client(self,destination):
-        # if self.bouteilles >= (destination.capacity-destination.stock_full):
-        #     self.bouteilles -= (destination.capacity-destination.stock_full)
-        #     destination.stock = destination.capacity
-        # else:
-        #     destination.stock = self.bouteilles+destination.stock_full
-        #     self.bouteilles = 0
+        if self.bouteilles >= (destination.capacity-destination.stock_full):
+            self.bouteilles -= (destination.capacity-destination.stock_full)
+            destination.stock = destination.capacity
+        else:
+            destination.stock = self.bouteilles+destination.stock_full
+            self.bouteilles = 0
 
 
     def new_destination(self):
