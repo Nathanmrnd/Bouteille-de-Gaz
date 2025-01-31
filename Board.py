ab8c357b5eb7bc5cb1f0c5f4582bf4f1a8804a14
Alphonse Raynaud <alphonse.raynaud@etu.minesparis.psl.eu>
2025-01-31 15:23:17 +0100

    carte de la france

--- Board.py
+++ Board.py
@@ -3,22 +3,28 @@ from plant import Plant
 from truck import Truck
 from client import Client
 
-yoffset = 4000
+xmin, xmax = -500, 1000
+ymin, ymax = 4300, 5000
+width, height = 1000, 1000
+def transform_x(x):
+    return (x-xmin)/(xmax-xmin)*width
+def transform_y(y):
+    return (ymax-y)/(ymax-ymin)*height
 class Board:
     def __init__(self,screen):
         self.color = (128,128,128)
         self.screen = screen
 
     def draw_plant(self,plant : Plant):
-        rect = pygame.Rect(plant.x, plant.y-yoffset, 10, 10)
+        rect = pygame.Rect(transform_x(plant.x), transform_y(plant.y), 10, 10)
         pygame.draw.rect(self.screen, (255,0,0) , rect)
 
     def draw_truck(self,truck: Truck):
-        rect = pygame.Rect(truck.x, truck.y-yoffset, 2, 2)
+        rect = pygame.Rect(transform_x(truck.x), transform_y(truck.y), 2, 2)
         pygame.draw.rect(self.screen, (0,255,0) , rect)
 
     def draw_client(self,client: Client):
-        rect = pygame.Rect(client.x, client.y-yoffset, 5, 5)
+        rect = pygame.Rect(transform_x(client.x), transform_y(client.y), 5, 5)
         pygame.draw.rect(self.screen, (0,0,255) , rect)
 
 
@@ -26,19 +32,13 @@ class Board:
         self.screen.fill(self.color)
         for plant in plants:    
             self.draw_plant(plant)
-            pygame.display.update()
         for client in clients:
             self.draw_client(client)
-            pygame.display.update()
-#peut-être réflechir à ne pas redessiner les usines/clients à chaque fois
         for truck in trucks:
             self.draw_truck(truck)
-            pygame.display.update()
 
-        pygame.display.update()
 
     def draw_t(self,trucks):
         for truck in trucks:
             self.draw_truck(truck)
 
-        pygame.display.update()
